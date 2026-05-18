a170e06b4256fd2a3be5afc0c4f6acb9a173e1ae
Subhrodip Mohanta <contact@subhrodip.com>
2026-05-18 20:43:18 +0200

    Initial Commit

---
+++ src/main/kotlin/sh/whoa/chitragupta/ChitraguptaApplication.kt
@@ -0,0 +1,11 @@
+package sh.whoa.chitragupta
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChitraguptaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChitraguptaApplication>(*args)
+}
---
+++ src/test/kotlin/sh/whoa/chitragupta/ChitraguptaApplicationTests.kt
@@ -0,0 +1,13 @@
+package sh.whoa.chitragupta
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChitraguptaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
